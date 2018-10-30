d86ceaf215dec8dc17bf1c9b5a0182f6fb5468a6
f2015537 <divyampatro1997@gmail.com>
2018-10-31 03:26:44 +0530

    Integratin with frontend

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 const database = {
     users: [
@@ -42,6 +45,9 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req,res) => {
     const { email, password, name } = req.body
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name,
