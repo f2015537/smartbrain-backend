2c7b90362241b6b5b29f8a49f161a1efc7f478b2
f2015537 <divyampatro1997@gmail.com>
2018-11-01 20:46:21 +0530

    Worked on the profile route

--- server.js
+++ server.js
@@ -68,14 +68,19 @@ app.post('/register', (req,res) => {
 
 app.get('/profile/:id', (req,res) => {
     const { id } = req.params
-    let found = false
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true
-            return res.json(user)
+    db
+    .select('*')
+    .from('users')
+    .where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
         }
-    })
-    if(!found) res.status(400).json("no such user")
+        else {
+            res.status(400).json('Not found')
+        }
+        })
+    .catch(err => res.status(400).json('error getting user'));
 })
 
 app.put('/image', (req,res) => {
